64433eb212210e76fd322a92e8b3d59fdef5c2c0
Sapheeaa <sophia_lukyanchik@mail.ru>
2025-11-10 23:12:48 +0300

    feat: add the most difficult task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -370,8 +370,9 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len);
+  return arr.fill(0).map((_, i) => 2 * i + 1);
 }
 
 /**
@@ -386,8 +387,11 @@ function generateOdds(/* len */) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  const result = indices.reduce((currentLevel, index) => {
+    return currentLevel[index];
+  }, arr);
+  return result;
 }
 
 /**
