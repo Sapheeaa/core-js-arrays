9edfd52f1b64988edd3015d2cff9b688b9f40006
Sapheeaa <sophia_lukyanchik@mail.ru>
2025-11-13 19:33:27 +0300

    feat: add more done tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -459,8 +459,13 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  const result = arr.map((item) => {
+    const strRes = item.toString(16).toUpperCase();
+    const fullZeroStr = strRes.padStart(6, '0');
+    return `#${fullZeroStr}`;
+  });
+  return result;
 }
 
 /**
